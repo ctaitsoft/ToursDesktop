4344b114d98da6fa18425ddebf736ab2c361244d
Morad_Tamimi <morad.al-tamimi@live.com>
2018-09-02 08:49:48 +0300

    Add project files.

---
+++ ToursDesktop/ViewModels/TourStopsViewModel.cs
@@ -0,0 +1,31 @@
+﻿using ToursDesktop.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ToursDesktop.ViewModels {
+	public class TourStopsViewModel : INotifyPropertyChanged {
+
+		public TourStopsViewModel() {
+
+			this.TourStops =  Models.TourSource.TourStops;
+		}
+
+
+
+		public void SelectionChanged(object sender, SelectionChangedEventArgs e) {
+		//	this.SelectedTour = e.AddedItems as TourStop;
+		}
+
+		public TourStop SelectedTour { get; set; }
+
+		public ObservableCollection<TourStop> TourStops { get; set; }
+
+		public event PropertyChangedEventHandler PropertyChanged;
+	}
+}
